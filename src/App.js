1c4c2785f30f44c4641cb5893ad681d4c9679ecc
threehundo <samuelbaik87@gmail.com>
2021-07-20 17:05:00 -0700

    Theme & ThemeProvider created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import '@fontsource/roboto';
+import { Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 import { About } from './components/About';
 import Footer from './components/Footer';
 import { Header } from './components/Header';
 import { Work } from './components/Work';
 
+const useStlyes = makeStyles({
+  root: {
+    height: "100vh",
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
+  }
+})
+
 function App() {
+  const classes = useStlyes();
   return (
-    <div className="App">
-      <Header className="App-header">
+    <div className={classes.root}>
+      <Button color="secondary" variant="contained">Hi</Button>
+      <Header className={classes.root}>
         {/* <Header /> */}
       </Header>
       <About id="about" />
--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#B60894"
+    },
+    secondary: {
+      main: "#E1AA7D"
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <ThemeProvider theme={theme}>  
+    <App /> 
+  </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
