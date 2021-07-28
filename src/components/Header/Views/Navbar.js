5a2610fa052ed198abf1dc5ba959adfd79f696fd
threehundo <samuelbaik87@gmail.com>
2021-07-28 16:49:42 -0700

    Navbar hamburger & close spans animation changed to in and out

--- src/components/Header/Views/Navbar.js
+++ src/components/Header/Views/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { theme, Nav, Container } from "../../../View/theme";
-// import ListItems from "./ListItems";
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
 const Navigator = styled(Nav)`
   background-color: transparent;
@@ -22,10 +21,6 @@ const NavbarContainer = styled(Container)`
 `;
 
 const NavMenu = styled.div`
-  animation: ${(props) =>
-    props.show
-      ? `slide-right 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both`
-      : `slide-right-2 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both`};
   background-color: transparent;
   position: absolute;
   top: 0;
@@ -35,33 +30,25 @@ const NavMenu = styled.div`
   display: flex;
   justify-content: space-around;
   flex-direction: column;
-  @keyframes slide-right {
-    0% {
-      transform: translateX(0);
-      opacity: 1;
-    }
-    100% {
-      transform: translateX(40px);
-      opacity: 0;
-    }
-  }
-  @keyframes slide-right-2 {
-    0% {
-      transform: translateX(-40px);
-      opacity: 0;
-    }
-    100% {
-      transform: translateX(0);
-      opacity: 1;
-    }
-  } ;
+  animation: ${(props) =>
+    props.show
+      ? css`
+          ${slideLeftHide} 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both
+        `
+      : css`
+          ${slideRightShow} 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both
+        `};
 `;
 
 const NavClose = styled.div`
   animation: ${(props) =>
     props.show
-      ? `slide-right-2 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both`
-      : `slide-right 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both`};
+      ? css`
+          ${slideRightShow} 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both
+        `
+      : css`
+          ${slideLeftHide} 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both
+        `};
   position: absolute;
   top: 0;
   left: 0;
@@ -69,26 +56,28 @@ const NavClose = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
-  @keyframes slide-right {
+`;
+
+const slideLeftHide = keyframes`
     0% {
       transform: translateX(0);
       opacity: 1;
     }
     100% {
-      transform: translateX(40px);
+      transform: translateX(-50px);
       opacity: 0;
     }
-  }
-  @keyframes slide-right-2 {
+`;
+
+const slideRightShow = keyframes`
     0% {
-      transform: translateX(-40px);
+      transform: translateX(-50px);
       opacity: 0;
     }
     100% {
       transform: translateX(0);
       opacity: 1;
     }
-  }
 `;
 
 const NavSpan = styled.span`
@@ -116,9 +105,6 @@ const Navbar = ({ show, handleClick }) => {
           <NavX />
         </NavClose>
       </NavbarContainer>
-      {/* <MenuContainer>
-        <ListItems show={show} handleClose={handleClose} />
-      </MenuContainer> */}
     </Navigator>
   );
 };
