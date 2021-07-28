df41d772bdaed47e6b8435b705cd87f1b5bf0e76
threehundo <samuelbaik87@gmail.com>
2021-07-27 17:13:18 -0700

    animation tied to animate state functional

--- src/components/Header/Views/ListItems.js
+++ src/components/Header/Views/ListItems.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled, { css } from "styled-components/macro";
-import { NavBlocks } from "../../../View/theme";
+import { NavBlocks, scaleUpTopLeft } from "../../../View/theme";
 
 // animate actual links
 // when navblocks is open links attrs to fullscreen
 // hide after
+
+// initial values
+// animate
+// reset to initial values
+// visibility = hidden
 
 /* 
   opacity: 1; 
   z-index: 0;
   transform: matrix(1, 0, 0, 1, 0, 0)
   transform: matrix(2, 0, 0, 2, 0, 0)
+  cubic-bezier(0.23, 1, 0.32, 1)
 */
-
+/* animate &&
+    css`
+      animation: 3s ${scaleUpTopLeft} cubic-bezier(0.39, 0.575, 0.565, 1)
+        forwards;
+    `} */
 const NavBlock = styled(Link)`
   position: absolute;
   width: 50%;
@@ -22,18 +32,28 @@ const NavBlock = styled(Link)`
   justify-content: center;
   align-items: center;
   opacity: 1;
-  transform: scale(1);
-  transition: transform 300ms cubic-bezier(0.23, 1, 0.32, 1);
-
-  ${(first) =>
-    first &&
-    css`
-      top: 0;
-      left: 0;
-      background-color: #fff;
-      transform-origin: top left;
-    `};
+  z-index: 0;
+  animation: ${(props) =>
+      props.animate
+        ? css`3s ${scaleUpTopLeft} cubic-bezier(0.39, 0.575, 0.565, 1)
+            forwards;`
+        : undefined}
+    ${(first) =>
+      first &&
+      css`
+        top: 0;
+        left: 0;
+        background-color: #fff;
+        transform-origin: top left;
+      `};
 `;
+
+/* animate &&
+    css`
+      animation: 3s ${scaleUpTopLeft} cubic-bezier(0.39, 0.575, 0.565, 1)
+        forwards;
+    `} */
+
 const NavBlock2 = styled(Link)`
   position: absolute;
   width: 50%;
@@ -42,6 +62,7 @@ const NavBlock2 = styled(Link)`
   justify-content: center;
   align-items: center;
   opacity: 1;
+  z-index: 0;
 
   ${(second) =>
     second &&
@@ -60,6 +81,7 @@ const NavBlock3 = styled(Link)`
   justify-content: center;
   align-items: center;
   opacity: 1;
+  z-index: 0;
 
   ${(third) =>
     third &&
@@ -92,45 +114,41 @@ const NavBlock4 = styled(Link)`
 
 const ListItems = ({ show, handleClose, first, second, third, fourth }) => {
   const links = ["Home", "About", "Work", "Contact"];
+  const [animate, setAnimate] = useState(false);
+  const startAnimationAndClose = () => {
+    setAnimate(!animate);
+    setTimeout(handleClose(), 500);
+    return console.log("Show: ", show);
+  };
+
   return (
     <NavBlocks show={show}>
-      <NavBlock to="/" first={first}>
-        <span onClick={() => handleClose()}>{links[0]}</span>
+      <NavBlock to="/" first={first} animate={animate}>
+        <span onClick={() => startAnimationAndClose()}>{links[0]}</span>
       </NavBlock>
-      <NavBlock2 to={`/${links[1].toLowerCase()}`} second={second}>
-        <span onClick={() => handleClose()}>{links[1]}</span>
+      <NavBlock2
+        to={`/${links[1].toLowerCase()}`}
+        second={second}
+        animate={animate}
+      >
+        <span onClick={() => startAnimationAndClose()}>{links[1]}</span>
       </NavBlock2>
-      <NavBlock3 to={`/${links[2].toLowerCase()}`} third={third}>
-        <span onClick={() => handleClose()}>{links[2]}</span>
+      <NavBlock3
+        to={`/${links[2].toLowerCase()}`}
+        third={third}
+        animate={animate}
+      >
+        <span onClick={() => startAnimationAndClose()}>{links[2]}</span>
       </NavBlock3>
-      <NavBlock4 to={`/${links[3].toLowerCase()}`} fourth={fourth}>
-        <span onClick={() => handleClose()}>{links[3]}</span>
+      <NavBlock4
+        to={`/${links[3].toLowerCase()}`}
+        fourth={fourth}
+        animate={animate}
+      >
+        <span onClick={() => startAnimationAndClose()}>{links[3]}</span>
       </NavBlock4>
     </NavBlocks>
   );
 };
-
-// const ListItems = ({ show, handleClose, first, second, third, fourth }) => (
-//   <NavBlocks show={show}>
-//     {links.map((option, index) => {
-//       let linkName, tile;
-//       option === "Home" ? (linkName = "") : (linkName = option.toLowerCase());
-//       option === "Home"
-//         ? (tile = first)
-//         : option === "About"
-//         ? (tile = second)
-//         : option === "Work"
-//         ? (tile = third)
-//         : (tile = fourth);
-//       return (
-//         <NavBlock key={index} to={`/${linkName}`} tile={tile}>
-//           <span to={`/${linkName}`} onClick={() => handleClose()}>
-//             {option}
-//           </span>
-//         </NavBlock>
-//       );
-//     })}
-//   </NavBlocks>
-// );
 
 export default ListItems;
